7f8b7ad579c86c45d1edb53d711b46d5b8971258
zsfer <seiferalbacete@gmail.com>
2025-06-05 20:02:18 +0800

    initial commit

---
+++ src/renderer.ts
@@ -0,0 +1,146 @@
+import { v4 as uuid } from 'uuid';
+
+import './index.css';
+
+type Task = {
+    id: string,
+    content: string,
+    category: string,
+    dateAdded: Date,
+    isDone: boolean
+    doneAt: Date,
+}
+
+let currentFilter: string = "";
+let savedTasks: Task[] = []
+let categories: string[] = []
+
+window.addEventListener('DOMContentLoaded', () => {
+    let tasks = loadTasks();
+
+    const input = document.getElementById("task-input") as HTMLInputElement;
+    const category = document.getElementById("task-category") as HTMLInputElement;
+
+    const form = document.getElementById("task-form") as HTMLFormElement;
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const val = input.value.trim()
+        if (val) {
+            const c = category.value.trim()
+            addTask(val, c);
+            render();
+
+            form.reset();
+        }
+    });
+
+    $( "#task-category" ).autocomplete({
+        source: categories
+    });
+
+    // render tasks
+    render();
+});
+
+// @Todo load from file
+function loadTasks(): Task[] {
+    return savedTasks
+}
+
+function addTask(content: string, category: string): [Task, boolean] {
+    if (content)
+    {
+        const t: Task = {
+            id: uuid(),
+            content: content,
+            dateAdded: new Date(),
+            isDone: false,
+            category: category,
+        }
+
+        if (category) {
+            if (!categories.includes(category)) {
+                categories.push(category)
+            }
+        }
+
+        savedTasks.push(t)
+        save();
+
+        return [t, true];
+    }
+
+    return [undefined, false];
+}
+
+function save(): boolean {
+    return false;
+}
+
+function render() {
+    loadTasks();
+
+    const list = document.getElementById("task-list")
+    list.innerHTML = '';
+    savedTasks.filter(t => currentFilter ? t.category === currentFilter : true).forEach((t, i) => {
+        const id = `TASK_${t.id}_${i}`;
+
+        const li = document.createElement('li');
+        li.className = "task-content"
+        li.id = id
+        li.innerHTML = `<input type="checkbox" /><div class="content">${t.content}</div>`;
+
+        if (t.category) {
+            const category = document.createElement('span')
+            category.className = 'category'
+            category.innerText = `#${t.category}`;
+            li.querySelector(".content").append(category)
+        }
+
+        const check = li.querySelector("input") as HTMLInputElement
+        check.checked = t.isDone
+        check.addEventListener('change', () => {
+            setTaskDone(t, check.checked)
+            render()
+        })
+
+        if (t.isDone) li.classList.add('done');
+
+        list.append(li)
+    })
+
+    const categoriesList = document.getElementById('categories');
+    categoriesList.innerHTML = '';
+    if (categoriesList.childElementCount != categories.length)
+    {
+        categories.forEach((c) => {
+            const btn = document.createElement('button') as HTMLButtonElement
+            btn.id = `${c}`;
+            btn.textContent = `#${c}`;
+            btn.addEventListener('click', e => {
+                e.preventDefault();
+                currentFilter = c;
+
+                render();
+            });
+
+            categoriesList.append(btn)
+        })
+
+    }
+
+    const clear = document.getElementById("clear-filter");
+    clear.addEventListener('click', () => {
+        currentFilter = '';
+        render();
+    })
+
+    clear.hidden = !currentFilter
+}
+
+function setTaskDone(task: Task, done: boolean)
+{
+    task.isDone = done
+    task.doneAt = done ? new Date() : null;
+}
